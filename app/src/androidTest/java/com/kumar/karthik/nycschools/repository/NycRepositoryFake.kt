f108a6e23cfda612411e621cef318455df6d1eef
manoflogan <manoflogan@gmail.com>
2024-01-01 14:54:03 -0800

    Updates KDocs (#2)

--- app/src/androidTest/java/com/kumar/karthik/nycschools/repository/NycRepositoryFake.kt
+++ app/src/androidTest/java/com/kumar/karthik/nycschools/repository/NycRepositoryFake.kt
@@ -5,12 +5,14 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.update
 
 import javax.inject.Inject
 import javax.inject.Singleton
 
+/**
+ * Test implementation that returns the preinitialised response instead of making network calls.
+ */
 @Singleton
 class NycRepositoryFake @Inject constructor(): NycSchoolRepository {
 
